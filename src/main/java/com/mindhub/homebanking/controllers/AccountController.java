9d75acaf41d29781851aaa91cd8c9bc99bf7f11a
Felipe <philbahia@hotmail.com>
2023-08-24 03:14:40 -0300

    Task6 Correction

--- src/main/java/com/mindhub/homebanking/controllers/AccountController.java
+++ src/main/java/com/mindhub/homebanking/controllers/AccountController.java
@@ -3,8 +3,12 @@ package com.mindhub.homebanking.controllers;
 import com.mindhub.homebanking.dtos.AccountDTO;
 import com.mindhub.homebanking.dtos.ClientDTO;
 import com.mindhub.homebanking.models.Account;
+import com.mindhub.homebanking.models.Client;
 import com.mindhub.homebanking.repositories.AccountRepository;
+import com.mindhub.homebanking.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,16 +27,29 @@ public class AccountController {
     @Autowired
     private AccountRepository accountRepository;
 
+    @Autowired
+    private ClientRepository clientRepository;
+
     @RequestMapping("/accounts")
     public List<AccountDTO> getAccounts(){
         return accountRepository.findAll().stream()
                 .map(AccountDTO::new).collect(toList());
     }
 
+//modif
 
     @RequestMapping("/accounts/{id}")
-    public AccountDTO getAccountById(@PathVariable Long id){
-        Optional<Account> account = accountRepository.findById(id);
-        return new AccountDTO(account.get());
+    public ResponseEntity<Object> getAccount(@PathVariable Long id, Authentication authentication){
+
+        Client client = clientRepository.findByEmail(authentication.getName());
+        Account account = accountRepository.findById(id).orElse(null);
+
+        if (account.getOwnerAccount().equals(client)){
+            AccountDTO accountDTO = new AccountDTO(account);
+            return new ResponseEntity<>(accountDTO,HttpStatus.ACCEPTED);
+        }else{
+            return new ResponseEntity<>("Account no Available",HttpStatus.CONFLICT);
+        }
+
     }
 }
