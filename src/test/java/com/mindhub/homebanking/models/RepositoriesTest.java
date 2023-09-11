fa9ee14b14b4332be280c7db00fef1d70425ff4b
Felipe <philbahia@hotmail.com>
2023-09-10 23:30:10 -0300

    cc

---
+++ src/test/java/com/mindhub/homebanking/models/RepositoriesTest.java
@@ -0,0 +1,43 @@
+package com.mindhub.homebanking.models;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import static org.hamcrest.Matchers.*;
+
+
+import com.mindhub.homebanking.repositories.LoanRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.lang.model.type.TypeKind;
+import java.util.List;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class RepositoriesTest {
+
+    @Autowired
+    LoanRepository loanRepository;
+
+
+
+    @Test
+    public void existLoans(){
+
+        List<Loan> loans = loanRepository.findAll();
+        assertThat(loans,is(not(empty())));
+}
+
+    @Test
+    public void existPersonalLoan(){
+
+        List<Loan> loans = loanRepository.findAll();
+        assertThat(loans, hasItem(hasProperty("name", is("Personal"))));
+
+    }
+
+
+
+}
